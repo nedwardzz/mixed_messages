8067bab13b4a6a096639a048e7f95bc3b902510a
Nick Edwards <nedwardzz@Nicks-Mac-mini.local>
2021-12-14 18:10:32 -0500

    Created javascript file and added 4 blank arrays to store program data

---
+++ mixedMessages.js
@@ -0,0 +1,11 @@
+/* MIXED MESSAGES CodeCademy
+    Author: Nick Edwards
+    Last Revision: December 14, 2021
+*/
+
+// Instantiating 4 Arrays
+// 3 for each piece of data, 1 to store all data arrays
+let mixedMessages = [];
+let reactions = [];
+let observations = [];
+let suggestions = [];
