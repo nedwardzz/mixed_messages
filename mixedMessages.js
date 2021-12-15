e524c4bb1ee587045975a3734a1fac9f060eef65
Nick Edwards <nedwardzz@Nicks-Mac-mini.local>
2021-12-14 21:30:31 -0500

    Added observations array

--- mixedMessages.js
+++ mixedMessages.js
@@ -20,3 +20,14 @@ reactions.push(`I can't believe this!`);
 reactions.push(`Guess what!`);
 mixedMessages.push(reactions);
 //console.log(reactions); // Uncomment to test reactions array additions
+
+// Filling observations with a few strings
+observations.push(`You're not looking so good!`);
+observations.push(`I think you need a shower!`);
+observations.push(`Did you wake up on the wrong side of the bed?`);
+observations.push(`You seem really chipper!`);
+observations.push(`Why are you so sweaty?`);
+observations.push(`Did you touch my drumset?`);
+observations.push(`You need to grow up!`);
+mixedMessages.push(observations);
+//console.log(observations); // Uncomment to test observations array additions
